ca05f79f78f16e54ff13898900caea0365fddb83
CesarOrduz <lexius.c52@gmail.com>
2022-09-15 00:49:07 -0500

    Cesar_Orduz

--- mvc-php/model/admin/agreg_usu.php
+++ mvc-php/model/admin/agreg_usu.php
@@ -9,6 +9,40 @@ $usua = mysqli_fetch_assoc($usuarios);
 
 
 ?>
+
+<?php
+if ((isset($_POST["btnguardar"])) && ($_POST["btnguardar"] == "fanadd")) 
+    { 
+        $tp = $_POST ['TIP_USU'];
+        $sqladd = "SELECT * FROM tipousuario WHERE tipo ='$tp' ";
+        $query = mysqli_query($mysqli,$sqladd);
+        $fila = mysqli_fetch_assoc($query);
+        if($fila)
+            {
+             echo '<script>alert (" El usuario ya existe ");</script>';
+             echo '<script>window.location="agreg_usu.php"</script>';
+            }
+        elseif ($_POST['TIP_USU'] == "")
+            {
+             echo '<script>alert (" Existen campos vacios  ");</script>';
+             echo '<script>window.location="agreg_usu.php"</script>';
+            }
+        else
+            {
+             $tp = $_POST ['TIP_USU'];
+             $sqladd = " INSERT INTO tipousuario(tipo) values ('$tp')";
+             $query = mysqli_query($mysqli,$sqladd);   
+             echo '<script>alert (" Registro Exitoso!!  ");</script>';
+             echo '<script>window.location="agreg_usu.php"</script>';
+            }
+        
+            
+    
+    }
+
+
+
+?>
 <form method="POST">
 
     <tr>
@@ -49,7 +83,7 @@ if(isset($_POST['btncerrar']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css">
-    <title>taller</title>
+    <title>Creacion Tipo de Usuario</title>
 </head>
     <body>
         <section class="title">
@@ -60,10 +94,26 @@ if(isset($_POST['btncerrar']))
                 <tr>
                     <td colspan="2">Tipos de Usuario</td>
                 </tr>
+                
                 <tr>
                     <td>Identificador</td>
-                    <td><input type="text"></td>
+                    <td><input type="text" readonly></td>
                 </tr>
+                
+                <tr>
+                    <!--// lowercase -->
+                    <td>Tipo Usuario</td>
+                    <td><input type="text" name="TIP_USU" placeholder="Ingrese tipo usuario" style="text-transform: uppercase;"></td>
+                </tr>
+                
+                <tr>
+                    <td colspan="2">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" name="btnadd" value="Guardar"></td>
+                    <input type="hidden" name="btnguardar" value="fanadd">
+                </tr>
+                
                 
             </form>
             
