11a1d3ab24caac0a5c4cae1d378cd07976bff7f6
SrToro <santoloza@hotmail.com>
2022-09-29 02:47:30 -0500

    ultima config view usu e index

--- mvc-php/model/admin/view_usu.php
+++ mvc-php/model/admin/view_usu.php
@@ -32,6 +32,7 @@ if(isset($_POST['btncerrar']))
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../../controller/image/iconoventana.png" type="image/x-icon">
         <link rel="stylesheet" href="estilos.css">
         <title>Usuarios registrados</title>
     </head>
